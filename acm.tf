5e32f5dacc0734d362c71b98affb9df2af87c6d8
nBrodie <brodiebaxter99@gmail.com>
2023-02-17 17:55:37 -0500

    website works now :^)

--- acm.tf
+++ acm.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "ssl_certificate" {
-#  provider                  = ssl
+  provider                  = aws.acm_provider
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
@@ -32,7 +32,11 @@ resource "aws_route53_record" "route53_record" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-#  provider                  = ssl
+  provider                  = aws.acm_provider
   certificate_arn = aws_acm_certificate.ssl_certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.route53_record : record.fqdn]
 }
+
+#resource "aws_route53_record" "route53_record" {
+#
+#}
--- provider.tf
+++ provider.tf
@@ -11,3 +11,9 @@ provider "aws" {
   profile = "default"
   region = var.region
 }
+
+provider "aws" {
+  alias = "acm_provider"
+  profile = "default"
+  region = "us-east-1"
+}
