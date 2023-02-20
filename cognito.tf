727db831a5274d27fb42c7aeb3a7254edce0d50b
nBrodie <brodiebaxter99@gmail.com>
2023-02-20 18:23:51 -0500

    Cognito set up, waiting on DNS record configuration to continue

---
+++ cognito.tf
@@ -0,0 +1,66 @@
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "user-pool"
+  username_attributes      = ["email"]
+  auto_verified_attributes = ["email"]
+
+  username_configuration {
+    case_sensitive = false
+  }
+
+  password_policy {
+    minimum_length = 8
+  }
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+    email_subject = "Account Confirmation"
+    email_message = "Your Upward Mobility confirmation code is {####}"
+  }
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name = "cognito-client"
+
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+  generate_secret = false
+  refresh_token_validity = 90
+  prevent_user_existence_errors = "ENABLED"
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH"
+  ]
+
+}
+
+resource "aws_route53_record" "auth-cognito-A" {
+  name    = aws_cognito_user_pool_domain.cognito-domain.domain
+  type    = "A"
+  zone_id = data.aws_route53_zone.route53_zone.zone_id
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cognito_user_pool_domain.cognito-domain.cloudfront_distribution_arn
+    # This zone_id is fixed
+    zone_id = "Z2FDTNDATAQYW2"
+  }
+}
+
+resource "aws_cognito_user_pool_domain" "cognito-domain" {
+  domain       = var.domain_name
+  certificate_arn = aws_acm_certificate.ssl_certificate.arn
+  user_pool_id = "${aws_cognito_user_pool.user_pool.id}"
+}
+
